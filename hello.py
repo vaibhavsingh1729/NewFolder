9a7a57446d3e2ab13db1eb50b8708bc65f9fa65b
vaibhavsingh1729 <vs027794@gmail.com>
2023-10-31 10:28:28 +0530

    Corrections made

--- hello.py
+++ hello.py
@@ -1,26 +1,40 @@
-def prit(n, isPrime):
+def sieve_of_eratosthenes(n):
+    isPrime = [True] * (n + 1)
     isPrime[0] = isPrime[1] = False
-    for i in range(2, n):
-        isPrime[i] = True
-    for p in range(2, n + 1):
-        if (p * p <= n and isPrime[p] == True):
+
+    p = 2
+    while p * p <= n:
+        if isPrime[p]:
             for i in range(p * 2, n + 1, p):
                 isPrime[i] = False
         p += 1
 
+    return isPrime
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def is_super_prime(num):
+    if is_prime(num):
+        while num > 0:
+            digit = num % 10
+            if not is_prime(digit):
+                return False
+            num //= 10
+        return True
+    return False
+
 def superPrimes(n):
-    isPrime = [1 for i in range(n + 1)]
-    prit(n, isPrime)
-    primes = [0 for i in range(2, n + 1)]
-    j = 0
-    for p in range(2, n + 1):
-        if (isPrime[p]):
-            primes[j] = p
-            j += 1
-    for k in range(j):
-        if (isPrime[k + 1]):
-            print(primes[k], end=" ")
+    isPrime = sieve_of_eratosthenes(n)
+    
+    super_primes = [p for p in range(2, n + 1) if isPrime[p] and is_super_prime(p)]
+
+    return super_primes
 
 n = 241
-print("Super-Primes less than or equal to", n, "are:")
-superPrimes(n)
+print("Super-Primes less than or equal to", n, "are:", superPrimes(n))
